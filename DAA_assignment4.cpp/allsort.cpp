9b64d2ca2a658baf74bba840abe6149e7010218f
Nischal <v.nischal1817@gmail.com>
2021-10-07 11:51:49 +0530

    modified assignment 4

--- DAA_assignment4.cpp/allsort.cpp
+++ DAA_assignment4.cpp/allsort.cpp
@@ -52,6 +52,10 @@ class sorting
                     }
                 }
                 swap(i,min_index);
+                //nischal 
+                //he is one of the most 
+                // most wonderful person
+                //oh nice
             }
         }
         void insertionSort()
